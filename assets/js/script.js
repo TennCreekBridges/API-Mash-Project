f7d9223153c63622aeef0a11a0c5829934da3911
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-01-25 09:53:38 -0600

    initialize architecture

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+// dad joke fetch and funciton
+//const apiUrl = "https://icanhazdadjoke.com";
+
+//var getApi = function () {
+    //fetch("https://icanhazdadjoke.com/", {
+        //headers: {
+          //Accept: "application/json"
+       // }
+      //})
+    //.then((response) => response.json())
+    //.then((data) => console.log(data))
+    //.catch((error) => console.log(error));
+//};
+//getApi();
+
+// Yoda fetch and function
+//const apiUrl = "http://yoda-api.appspot.com/api/v1/yodish";
+
+//var getApi = function () {
+    //fetch('http://yoda-api.appspot.com/api/v1/yodish')
+   // .then((response) => response.json())
+   // .then((data) => console.log(data));
+//}
+
+//getApi();
